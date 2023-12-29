d06ee0cb7ff467dcdf823b5684f76ba4c51e428e
Ziga Benko <zigac.benko@gmail.com>
2023-12-29 15:04:10 +0100

    Add image on repo list

--- resources/js/Pages/Repositories/Index.vue
+++ resources/js/Pages/Repositories/Index.vue
@@ -28,7 +28,7 @@ defineProps({
                                 <li v-for="repository in repositories" :key="repository.id" class="py-3 sm:py-4">
                                     <div class="flex items-center">
                                         <div class="flex-shrink-0">
-                                            <!-- <img class="w-8 h-8 rounded-full" :src="issue.user.avatar_url" alt="Neil image"> -->
+                                            <img class="w-8 h-8 rounded-full" :src="repository.user_avatar" alt="Neil image">
                                         </div>
                                         <div class="flex-1 min-w-0 ms-4">
                                             <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
