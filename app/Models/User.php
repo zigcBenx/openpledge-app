d1c5e9eeb64b9945b26790f78842a17450b51f5b
zigcBenx <zigac.benko@gmail.com>
2024-05-05 16:27:35 +0200

    Add programming languages relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function programmingLanguages()
+    {
+        return $this->morphToMany(ProgrammingLanguage::class, 'programming_languageable');
+    }
 }
