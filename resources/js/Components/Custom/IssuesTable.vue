dff32f55036482c4871be72afb4b3dc2b7c3de1a
zigcBenx <zigac.benko@gmail.com>
2024-07-02 19:09:46 +0200

    Create separate components for one issue entry

--- resources/js/Components/Custom/IssuesTable.vue
+++ resources/js/Components/Custom/IssuesTable.vue
@@ -26,100 +26,7 @@
               'dark:bg-rich-black bg-ghost-white': issue.state === 'closed'
             }]"
           >
-              <td :class="['rounded-bl-md font-medium overflow-hidden border-l-[6px] border-ocean-green dark:border-green pl-3.5 py-6 rounded-tl-md', {
-                '!border-tundora': issue.state === 'closed'
-              }]">
-                <div 
-                  :class="['text-ocean-green dark:text-green', {
-                    '!text-spun-pearl': issue.state === 'closed'
-                  }]"
-                >
-                  {{ issue.state }}
-                </div>
-              </td>
-              <td class="py-6">
-                <Link v-if="pledged"
-                    :class="['dark:text-white dark:hover:text-green hover:text-green text-base pr-4', {
-                      '!text-spun-pearl': issue.state === 'closed'
-                    }]"
-                    :href="'/issues/' + issue.id"
-                    >
-                    {{ issue.title }}
-                  </Link>
-                  <a
-                    v-else
-                    :href="issue.github_url"
-                    :class="['dark:text-white dark:hover:text-green hover:text-green text-base pr-4', {
-                      '!text-spun-pearl': issue.state === 'closed'
-                    }]"
-                    >
-                      {{ issue.title }}
-                  </a>
-                <a v-if="pledged" :href="issue.github_url" target="_blank"><i class="fa-brands fa-github"/></a>
-                
-                <div class="flex gap-1 mt-3">
-                  <Avatar :url="issue.user_avatar" size="sm" />
-                  <span class="dark:text-spun-pearl text-tundora text-xs font-medium">{{ issue.github_username }}</span>
-                  <span class="dark:text-spun-pearl text-tundora text-xs font-light">{{ dayjs(issue.created_at).fromNow() }}</span>
-                </div>
-              </td>
-              <td class="py-6 pr-4 align-middle">
-                <div class="flex flex-wrap gap-1">
-                  <!-- <Pill
-                    v-if="issue.labels && issue.labels.length > 0"
-                    v-for="label in issue.labels.split(',')"
-                    :key="label"
-                    color="present"
-                    size="sm"
-                    :disabled="issue.state === 'closed'"
-                  >
-                    {{ label }}
-                  </Pill> -->
-                </div>
-              </td>
-              <td class="py-6"><span class="dark:text-white font-medium text-xs pr-4">strapi/strapi</span></td>
-              <td class="py-6 pr-4 align-middle">
-                  <div class="flex flex-wrap gap-1">
-                    <Pill 
-                      v-if="issue.programming_languages"
-                      v-for="issue_lang in issue.programming_languages" 
-                      :key="issue_lang"
-                      color="present" 
-                      size="sm" 
-                      :disabled="issue.state === 'closed'"
-                    >
-                      {{ issue_lang.name }}
-                    </Pill>
-                    <Pill 
-                      v-else-if="issue.repository"
-                      v-for="lang in issue.repository.programming_languages" 
-                      :key="lang"
-                      color="present" 
-                      size="sm" 
-                      :disabled="issue.state === 'closed'"
-                    >
-                      {{ lang.name }}
-                    </Pill>
-                  </div>
-              </td>
-              <td class="py-6">
-                <span 
-                  :class="['text-purple-heart font-medium', {
-                    '!dark:text-spun-pearl text-tundora': issue.state === 'closed'
-                  }]"
-                >
-                  {{ issue.donations_sum_amount ?? 0 }} €
-                </span>
-              </td>
-              <td class="rounded-br-md rounded-tr-md pr-6">
-                  <Icon 
-                    name="star"
-                    width="1.375rem"
-                    :class="getIconStrokeColor(issue.favorite, issue.state === 'closed')"
-                    :disabled="issue.state === 'closed'"
-                    @click="addFavorites(issue)"
-                  />
-              </td>
+              <IssueItemPledged :issue="issue"/>
           </tr>
           <tr v-intersection-observer="onIntersectionObserver"></tr>
       </tbody>
@@ -127,13 +34,10 @@
 </template>
 
 <script setup>
-    import { Link } from '@inertiajs/vue3';
-    import Pill from '@/Components/Form/Pill.vue';
-    import Icon from '@/Components/Icon.vue';
-    import Avatar from '@/Components/Avatar.vue';
-    import { useDark } from '@vueuse/core';
+    import Icon from '@/Components/Icon.vue'
+    import Avatar from '@/Components/Avatar.vue'
     import { vIntersectionObserver } from '@vueuse/components'
-    import dayjs from '@/libs/dayjs';
+    import IssueItemPledged from '@/Components/Custom/IssueItemPledged.vue'
 
     defineProps({
         issues: {
@@ -142,40 +46,24 @@
             default: [],
             validator: function (value) {
                 return value.every(item => {
-                  return typeof item === 'object' && item.hasOwnProperty('id');
+                  return typeof item === 'object' && item.hasOwnProperty('id')
                 });
             }
         },
         pledged: {
           type: Boolean,
-          required: true,
+          required: false,
           default: () => {
             return false
           }
         }
-    });
+    })
 
-    const isDark = useDark();
-    const emit = defineEmits(['onLazyLoading']);
- 
-    const getIconStrokeColor = (isFavorite, isDisabled) => {
-      if (isDark.value) {
-        if (isDisabled) return 'dark:stroke-mondo dark:fill-mondo';
-        return isFavorite ? 'dark:stroke-green dark:fill-green' : 'dark:stroke-spun-pearl dark:hover:stroke-green';
-      }
-      if (isDisabled) {
-        return 'stroke-spun-pearl dark:fill-spun-pearl';
-      }
-      return isFavorite ? 'stroke-ocean-green fill-ocean-green' : 'stroke-tundora hover:stroke-ocean-green';
-    }
+    const emit = defineEmits(['onLazyLoading'])
 
     const onIntersectionObserver = ([{ isIntersecting }]) => {
       if (isIntersecting) {
         emit('onLazyLoading');
       }
-    }
-
-    const addFavorites = (issue) => {
-      issue.favorite = !issue.favorite;
     }
 </script>
