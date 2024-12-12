59fd7d5d12655e2065146d75c8ebaaff3e1f5a16
Kilian Trunk <kilian@ttsolutions.si>
2024-12-12 08:38:55 +0100

    fix favorite repositories pledged issues count

--- app/Actions/Favorite/GetFavoriteRepositories.php
+++ app/Actions/Favorite/GetFavoriteRepositories.php
@@ -19,7 +19,9 @@ class GetFavoriteRepositories
             'githubInstallation',
         ])
             ->withSum('donations', 'amount')
-            ->withCount('issues as pledged_issues_count')
+            ->withCount(['issues as pledged_issues_count' => function ($query) {
+                $query->whereHas('donations');
+            }])
             ->whereIn('id', $favoriteRepositoryIds)
             ->take(2)
             ->get()
@@ -43,7 +45,9 @@ class GetFavoriteRepositories
             'githubInstallation',
         ])
             ->withSum('donations', 'amount')
-            ->withCount('issues as pledged_issues_count')
+            ->withCount(['issues as pledged_issues_count' => function ($query) {
+                $query->whereHas('donations');
+            }])
             ->whereIn('id', $favoriteRepositoryIds);
 
         $repositories = $query->paginate($perPage, $columns, $pageName, $page);
