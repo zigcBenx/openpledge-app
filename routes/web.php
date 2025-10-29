f619506d09f6726af7a8ebcf71d54a8a1768c252
Ziga Benko <zigac.benko@gmail.com>
2025-10-29 14:24:36 +0100

    Remove donation controller

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\GithubController;
 use App\Http\Controllers\IssueController;
 use App\Http\Controllers\SearchController;
-use App\Http\Controllers\DonationController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\PaymentController;
@@ -77,7 +76,6 @@ Route::middleware([
 
     Route::resource('repositories', RepositoryController::class)->only('index', 'store');
     Route::resource('issues', IssueController::class)->only(['store'])->except(['show']);
-    Route::resource('donations', DonationController::class)->only('index', 'show', 'store');
 
 
     Route::get('issues/{issue}/donations', [IssueController::class, 'donations'])->name('issues.donations');
